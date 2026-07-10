eaed6013fe6c2b1610d8ad8d0505a1b3888523dc
Yash Masankhamb <yashmasankhamb4@gmail.com>
2026-07-10 12:51:02 +0530

    Time: 141 ms (5.01%), Space: 43.9 MB (5.01%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void printpermute(int index,vector<int>nums,set<vector<int>>& s)
+    {
+        if(index==nums.size())//base case
+        {
+            s.insert(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();i++)
+        {
+            swap(nums[index],nums[i]);
+            printpermute(index+1,nums,s);
+            swap(nums[index],nums[i]);
+                    
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        // i am using an optimal approach to the permutations problem
+        //that do not uses the map but ill be using a set to store only unique arrays
+        set<vector<int>> s;//stores the ans
+        printpermute(0,nums,s);
+        //set to vector conversion
+        vector<vector<int>>ans(s.begin(),s.end());
+        return ans;
+
+        
+    }
+};
